fae86157068183f0d4cece0a43ccaf249ccaf1a0
Nicholas Taylor <nicholas_taylor@me.com>
2021-05-23 22:37:00 -0400

    Adding a new source

--- graylady.js
+++ graylady.js
@@ -1,24 +1,60 @@
-const gotham = () => {
-  let caughtWall = false;
-  const gateway = document.querySelector('#gateway-content');
-  if (gateway)
-    {
-      gateway.remove();
-      caughtWall = true;
-    }
-  const content = document.querySelector('[class^="css-"]');
-  content.style.position = 'initial';
-  content.style.overflow = 'initial';
-  const rmGrad = (ele) => {
-    if (ele.className.startsWith('css-') && ele.style){
-      ele.style.display = 'none';
-    }
-  }
-  content.childNodes.forEach(ele => rmGrad(ele))
-  if (caughtWall){
-    console.log('Got it!')
-    document.removeEventListener('scroll',gotham);
-  }
+const helperSnap = (query, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(document.querySelector(query).innerHTML);
+    },delay)
+  })
 }
 
-document.addEventListener('scroll',gotham);
+const helperLoad = (query, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (document.querySelector(query)){
+        resolve(document.querySelector(query));
+      }
+    },delay)
+  })
+}
+
+const district = () => {
+  const takeSnapshot = helperSnap('article',200);
+  const paywallLoaded = helperLoad('[id^="paywall"]',2500);
+
+  const rmPaywall = (snapshot, paywall) => {
+    const body = document.querySelector('body');
+    const html = document.querySelector('html');
+    paywall.remove();
+    body.style.position = 'initial';
+    body.style.overflow = 'initial';
+    html.style.overflow = 'initial';
+    document.querySelector('article').innerHTML = snapshot;
+  }
+  
+  Promise.all([takeSnapshot,paywallLoaded]).then((allValues) => rmPaywall(allValues[0],allValues[1]));
+}
+
+const gotham = () => {
+  const paywallLoaded = helperLoad('#gateway-content',3000);
+
+  const rmPaywall = (paywall) => {
+    paywall.remove();
+    const content = document.querySelector('[class^="css-"]');
+    content.style.position = 'initial';
+    content.style.overflow = 'initial';
+    const rmGrad = (ele) => {
+      if (ele.className.startsWith('css-') && ele.style){
+        ele.style.display = 'none';
+      }
+    }
+    content.childNodes.forEach(ele => rmGrad(ele));
+  }
+
+  Promise.all([paywallLoaded]).then((allValues) => rmPaywall(allValues[0]));
+}
+
+const theHost = window.location.hostname;
+if (theHost === 'www.washingtonpost.com'){
+  district();
+} else if (theHost === 'www.nytimes.com'){
+  gotham();
+}
